f848e841a668873b6910dc0dbe73e24f05d27ab2
Arthur <arthurzhuk@gmail.com>
2022-10-08 17:21:26 -0700

    basic challenge

--- basic.ts
+++ basic.ts
@@ -1,25 +1,14 @@
-// connectToDatabase()
-// .then((database)  => {
-//     return getUser(database, 'email@email.com')
-//     .then(user => {
-//         return getUserSettings(database, user.id)
-//         .then(settings => {
-//             return setRole(database, user.id, "ADMIN")
-//             .then(success => {
-//                 return notifyUser(user.id, "USER_ROLE_UPDATED")
-//                 .then(success => {
-//                     return notifyAdmins("USER_ROLE_UPDATED")
-//                 })
-//             })
-//         })
-//     })
-// })
-
 const connectToDb = async () => {
-  const database = await connectToDatabase()
-  const user = await getUser(database, "email@email.com")
-  await getUserSettings(database, user.id)
-  await setRole(database, user.id, "ADMIN")
-  await notifyUser(user.id, "USER_ROLE_UPDATED")
-  await notifyAdmins("USER_ROLE_UPDATED")
+  const USER_ROLE_UPDATED = "USER_ROLE_UPDATED" // const to avoid magic strings
+
+  try {
+    const database = await connectToDatabase()
+    const user = await getUser(database, "email@email.com")
+    await getUserSettings(database, user.id)
+    await setRole(database, user.id, "ADMIN")
+    await notifyUser(user.id, USER_ROLE_UPDATED)
+    await notifyAdmins(USER_ROLE_UPDATED)
+  } catch (err) {
+    console.error(err) // Ideally notify user of error in some manner
+  }
 }
